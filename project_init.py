f85e6cda8e1b37bfd6cd3e20787f032b2a7ea38a
Andrei Lupascu <andrei.lupascu@hotmail.be>
2025-05-29 09:27:56 +0200

    auto commit

---
+++ project_init.py
@@ -0,0 +1,152 @@
+import os
+import shutil
+import platform
+import subprocess
+from pathlib import Path
+from datetime import datetime
+from typing import List, Tuple
+
+
+
+# CONFIG------------------------------------------------------------------------
+SUPPORTED_OS = ("Windows", "Linux")
+WORKING_DIR = Path.cwd()
+PROJECT_ARTIFACTS = []
+INITIALIZATION_TIME = None
+OPERATING_SYSTEM = None
+
+# folders
+DOCUMENTS_FOLDER_NAME = "documents"
+ASSETS_FOLDER_NAME = "assets"
+MODULES_FOLDER_NAME = "helpers"
+SCRIPTS_FOLDER_NAME = "scripts"
+TESTS_FOLDER_NAME = "tests"
+DEFAULT_FOLDERS_LIST = [
+    DOCUMENTS_FOLDER_NAME,
+    ASSETS_FOLDER_NAME,
+    MODULES_FOLDER_NAME,
+    SCRIPTS_FOLDER_NAME,
+    TESTS_FOLDER_NAME
+]
+
+# files
+
+
+# routing
+GIT_AUTO_ROUTING = {
+    "Linux": {
+        "extension": ".sh",
+        "content": """
+            #!/bin/bash
+
+            # add all changes
+            git add .
+
+            # commit (with default message if none provided)
+            commit_message=${1:-"auto commit"}
+            git commit -m "$commit_message"
+
+            # push to remote
+            git push
+            """
+    },
+    "Windows": {
+        "extension": ".bat",
+        "content": """
+            @echo off
+            REM add all changes
+            git add .
+
+            REM set commit message
+            set commit_message=%1
+            if "%commit_message%"=="" set commit_message=auto commit
+
+            REM commit
+            git commit -m "%commit_message%"
+
+            REM push
+            git push
+        """
+    }
+}
+
+# LOGIC-------------------------------------------------------------------------
+def create_folders(folders_list: list=DEFAULT_FOLDERS_LIST) -> None:
+    for folder_name in folders_list:
+        folder_path = WORKING_DIR / folder_name
+        folder_path.mkdir(exist_ok=True)
+        print(f"---Created: {folder_path}")
+
+def snapshot_initial_state() -> Tuple[datetime, List[str]]:
+    """Take a snapshot of the initial project (will be used to reverse changes)."""
+    return datetime.now(), [path.resolve() for path in WORKING_DIR.iterdir()]
+
+def reverse_init() -> None:
+    """
+    Reverses initialization changes (addition of folders and files)
+    WARNING: this function should ONLY be used on project initialization as
+    it doesn't keep track of additional user changes done throughout the project lifespan.
+    """
+
+    user_input = input(
+f"""
+--- This action will delete all content except the following items:
+
+{os.linesep.join([str(path) for path in PROJECT_ARTIFACTS])}
+
+Do you wish to continue? (y/n)
+"""
+)
+
+    # TODO: handle files in-use
+    if user_input == "y":
+        for item in WORKING_DIR.iterdir():
+            if item not in PROJECT_ARTIFACTS:
+                # remove directory
+                try:
+                    if item.is_dir():
+                        shutil.rmtree(item)
+                    else:
+                        item.unlink()
+                except Exception as e:
+                    print("Failed to delete {item}: {e}")
+    else:
+        print("---Deletion cancelled.")
+
+def detect_os():
+    os_name = platform.system()
+
+    if os_name in SUPPORTED_OS:
+        return os_name
+    else:
+        return None
+    
+def create_git_auto(git_extension: str, git_content: str) -> None:
+    """Creates a git_auto.ext script in the "scripts" folder."""
+    # adding the newline arg for the special kid: Windows –
+    # it automatically adds a "\r" to "\n", leading to duplicate newlines
+    # reason? historical (carriage return). couldn't be bothered to investigate
+    # Yes Bobby, you're daddy's 'special' little boy.
+    filename = WORKING_DIR / SCRIPTS_FOLDER_NAME / f"git_auto{git_extension}"
+    with open(filename, "w", newline="") as git_auto_script:
+        git_auto_script.write(git_content)
+
+def format_content(text: str) -> str:
+    """Remove leading spaces from a text."""
+    formatted_text = os.linesep.join(line.lstrip() for line in text.strip().splitlines())
+    return formatted_text
+
+# RUN---------------------------------------------------------------------------
+if __name__ == "__main__":
+    INITIALIZATION_TIME, PROJECT_ARTIFACTS = snapshot_initial_state()
+    OPERATING_SYSTEM = detect_os()
+    # get get git auto script extension and content based on OS
+    git_extension, git_content = (
+        GIT_AUTO_ROUTING[OPERATING_SYSTEM]["extension"],
+        format_content(GIT_AUTO_ROUTING[OPERATING_SYSTEM]["content"])
+    )
+    print(repr(git_content))
+    print(git_content)
+    create_folders()
+    create_git_auto(git_extension, git_content)
+    # reverse_init()
